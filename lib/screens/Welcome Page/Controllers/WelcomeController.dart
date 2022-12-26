7225b6de8f0107c2d328669aa7a0ee2f02253229
Alisina Barghandan <barghandanalisina@gmail.com>
2022-12-27 02:29:37 +0530

    creating the slider - first step - building template

--- lib/screens/Welcome Page/Controllers/WelcomeController.dart
+++ lib/screens/Welcome Page/Controllers/WelcomeController.dart
@@ -1,5 +1,5 @@
 import 'package:get/get.dart';
 
 class WelcomeController extends GetxController {
-  var msg = "Welcome page is very good".obs;
+  // var msg = "Welcome page is very good".obs;
 }
--- lib/screens/Welcome Page/WelcomePage.dart
+++ lib/screens/Welcome Page/WelcomePage.dart
@@ -1,26 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:sample/constants/constants.dart';
-import 'package:sample/screens/Welcome%20Page/Controllers/WelcomeController.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final welcomeData = Get.put(WelcomeController());
-
     return SafeArea(
-        child: Scaffold(
-      appBar: AppBar(),
-      body: Container(
-        width: 300,
-        height: 400,
-        decoration: BoxDecoration(color: BACKGROUND_COLOR),
-        child: Center(
-          child: Text("${welcomeData.msg}"),
+      child: Scaffold(
+        body: PageView.builder(
+          itemCount: 3,
+          scrollDirection: Axis.vertical,
+          itemBuilder: (_, index) {
+            return Container(
+              width: double.maxFinite,
+              height: double.maxFinite,
+              child: const Text(""),
+            );
+          },
         ),
       ),
-    ));
+    );
   }
 }
